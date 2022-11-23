858e934c66896ce8737aa17889f7b34629ecd26c
STEVENPC\Steven <stevenfgc@hotmail.com>
2022-11-23 13:17:38 -0500

    Initialize project using Create React App

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { NativeBaseProvider, extendTheme } from "native-base";
+import reportWebVitals from "./reportWebVitals";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+  },
+});
+
+// extend the theme
+type MyThemeType = typeof theme;
+declare module "native-base" {
+  interface ICustomTheme extends MyThemeType {}
+}
+ReactDOM.render(
+  <React.StrictMode>
+    <NativeBaseProvider theme={theme}>
+      <App />
+    </NativeBaseProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
